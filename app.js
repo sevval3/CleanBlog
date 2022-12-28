9fa7e221e045e827bf2ffa690096cf1d9b629435
sevval3 <E-posta>
2022-12-28 15:19:49 +0300

    new commit

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require('express');
-
+const path = require('path');
+const ejs = require('ejs');
 const app = express();
 
-app.get('/', (req, res) => {
+//VİEW ENGİNE
+app.set('view engine', 'ejs');
 
-  const blog = {
-    id: 1,
-    name: "Blog title",
-    description: "Blog description"
-  }
-  res.send(blog)
-})
+//MİDDLEWARE
+app.use(express.static('public'));
+
+//ROUTESc
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
+
+
 
