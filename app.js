6313c2a1224e5f315455b7a99c747acd244ab858
sevval3 <E-posta>
2023-01-10 16:39:57 +0300

    post

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
+const Post = require('./models/Post');
 const app = express();
+
+mongoose.connect('mongodb://192.168.152.172:27017/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 
 //VİEW ENGİNE
 app.set('view engine', 'ejs');
 
+
 //MİDDLEWARE
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
-//ROUTESc
-app.get('/', (req, res) => {
-  res.render('index');
-});
+//ROUTES
+app.get('/', async (req, res) => {
+    const posts = await Post.find({})
+    res.render('index',{
+      posts
+    });
+  });
 
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.get('/add', (req, res) => {
-  res.render('add');
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
+
+app.post('/posts', async (req, res) => {
+   await Post.create(req.body);
+    res.redirect('/');
+  });
 
 
 const port = 3000;
